23a31bf3fb267e02d0aa5ab125954afbef6ff72c
wlsp <wlsp@outlook.de>
2025-05-27 12:22:53 +0200

    disabling unused methode

--- backend/src/main/java/wlsp/tech/backend/security/SecurityConfig.java
+++ backend/src/main/java/wlsp/tech/backend/security/SecurityConfig.java
@@ -10,7 +10,7 @@ import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
+//import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
