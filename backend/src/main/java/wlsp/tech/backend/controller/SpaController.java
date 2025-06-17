b221e917ee3d9d5eb7b5f86f61b2aba434932b88
wlsp <81742640+wlsp@users.noreply.github.com>
2025-06-17 22:25:03 +0200

    Removed unused path variable in `SpaController` routing method for cleaner implementation.

--- backend/src/main/java/wlsp/tech/backend/controller/SpaController.java
+++ backend/src/main/java/wlsp/tech/backend/controller/SpaController.java
@@ -1,14 +1,13 @@
 package wlsp.tech.backend.controller;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class SpaController {
 
   @RequestMapping(value = {"/{path:^(?!api$|assets$|static$|.*\\..*).*}/**",})
-  public String forwardToClientRouter(@PathVariable String path) {
+  public String forwardToClientRouter() {
     return "forward:/index.html";
   }
 }
