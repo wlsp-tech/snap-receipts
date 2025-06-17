cb4a35014bf2fca30ba42f2ead2f8f1f3cb67517
wlsp <81742640+wlsp@users.noreply.github.com>
2025-06-17 21:06:05 +0200

    Updated frontend build workflow name for SonarQube integration, added project key and organization to SonarQube properties, adjusted artifact name in deploy workflow, and enhanced `SpaController` routing with path variable support.

--- backend/src/main/java/wlsp/tech/backend/controller/SpaController.java
+++ backend/src/main/java/wlsp/tech/backend/controller/SpaController.java
@@ -1,13 +1,14 @@
 package wlsp.tech.backend.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class SpaController {
 
   @RequestMapping(value = {"/{path:^(?!api$|assets$|static$|.*\\..*).*}/**",})
-  public String forwardToClientRouter() {
+  public String forwardToClientRouter(@PathVariable String path) {
     return "forward:/index.html";
   }
 }
