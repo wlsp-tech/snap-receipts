04ea2a8a18438e9ab5ab2cfd0ee9796d966edd96
wlsp <81742640+wlsp@users.noreply.github.com>
2025-06-18 21:11:30 +0200

    fixing bugs

--- frontend/src/components/ui/qrcode.tsx
+++ frontend/src/components/ui/qrcode.tsx
@@ -1,7 +1,10 @@
 import {QRCode} from "react-qrcode-logo";
 
 const QRCodeComp = ({token}: { token?: string }) => {
-    const url = `${import.meta.env.VITE_FRONTEND_ORIGIN}/document-upload/receipt/${token}`;
+    const FRONTEND_ORIGIN =
+        import.meta?.env?.VITE_FRONTEND_ORIGIN || (typeof process !== 'undefined' ? process.env.VITE_FRONTEND_ORIGIN : '');
+    const url = `${FRONTEND_ORIGIN}/document-upload/receipt/${token}`;
+
     return <QRCode
         value={url}
         size={150}
